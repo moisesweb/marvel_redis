dc9437a60c2594a15858bd3c4321c75f4c3adbda
Moises Vargas M <moisesweb@gmail.com>
2015-07-08 22:56:17 -0400

    solution part2

--- app/models/metrics.rb
+++ app/models/metrics.rb
@@ -1,21 +1,32 @@
 class Metrics
+  attr_reader :weapon
 
   def initialize
+    @weapon = DataStructures.get
   end
 
   def login_count_for(name)
+    weapon.zincrby('users_logins', 1, name)
   end
 
   def count_login(name)
+    weapon.zscore('users_logins', name)
   end
 
   def top_five_login
+    weapon.zrevrange('users_logins', 0, 4)
   end
 
   def track_online(name)
+    key  = Time.now.to_i
+    weapon.sadd(key, name).tap do
+      weapon.expire(key, 60 * 5)
+    end
   end
 
   def users_online
+    time_range = (1.minute.ago.to_i..Time.now.to_i).to_a
+    weapon.sunion(time_range)
   end
 end
 
